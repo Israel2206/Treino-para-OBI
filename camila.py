739a4009c347d96f2e528ae6413f141f0de15c71
Israel <brenoisrael931@gmail.com>
2025-06-09 22:07:14 -0300

    OBI

---
+++ camila.py
@@ -0,0 +1,13 @@
+idade = []
+
+idade1 = int(input())
+idade2 = int(input())
+idade3 = int(input())
+
+idade.append(idade1)
+idade.append(idade2)
+idade.append(idade3)
+
+idade.sort()
+
+print(idade[1])
